527764eb48798f5729ade163aa9bd8e9721ae6b0
arunsiv <arunsiv>
2017-04-18 15:20:53 -0400

    added timestamp

--- public/js/index.js
+++ public/js/index.js
@@ -10,22 +10,22 @@ socket.on('disconnect', function() {
 
 //Listener for new message
 socket.on('newMessage', function(message) {
-    console.log('New Message: ', message);
+    var formattedTime = moment(message.createdAt).format('h:mm a');
 
     var li = jQuery('<li></li>');
-    li.text(`${message.from}: ${message.text}`);
+    li.text(`${message.from} ${formattedTime}: ${message.text}`);
 
     jQuery('#messages').append(li);
 });
 
 //Listener for new location message
 socket.on('newLocationMessage', function(message) {
-    console.log('New Location Message: ', message);
+    var formattedTime = moment(message.createdAt).format('h:mm a');
 
     var li = jQuery('<li></li>');
     var a = jQuery('<a target="_blank">My current location</a>')
 
-    li.text(`${message.from}: `);
+    li.text(`${message.from} ${formattedTime}: `);
     a.attr('href', message.url);
     li.append(a);
     jQuery('#messages').append(li);
@@ -40,7 +40,6 @@ jQuery('#message-form').on('submit', function(e) {
         from: 'User',
         text: messageTextBox.val()
     }, function() {
-        console.log('Ack from server');
         messageTextBox.val('');
     });
 });
